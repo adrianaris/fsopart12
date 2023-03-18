ae5f1afc04b87b36d45b534eb5de5f3497d25155
Adrian Serbanescu <adrian@adrianserbanescu.com>
2023-03-18 16:44:45 +0100

    ex12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const redis = require('../redis')
+const redis = require('../redis');
 
-const configs = require('../util/config')
+const configs = require('../util/config');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
@@ -17,12 +17,10 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/statistics', async (_, res) => {
-  console.log('been here')
   const stats = await redis.getAsync('added_todos')
-  console.log(stats)
   res.json({
-    added_todos: stats
-  })
+    added_todos: stats ? stats : 0
+  });
 })
 
 module.exports = router;
